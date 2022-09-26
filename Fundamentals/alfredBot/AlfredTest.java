bd2b342e93746434fa92b8bf1e7a5cf7698d8683
cdejonge89 <cdejonge89@gmail.com>
2022-09-26 11:57:51 -0700

    alfredBot update

--- Fundamentals/alfredBot/AlfredTest.java
+++ Fundamentals/alfredBot/AlfredTest.java
@@ -1,14 +1,32 @@
 class AlfredTest {
     public static void main(String[] args) {
         AlfredQuotes alfredBot = new AlfredQuotes();
-        System.out.println(alfredBot.basicGreeting());
-        System.out.println(alfredBot.guestGreeting("Lee", "morning"));
-        System.out.println(alfredBot.dateAnnouncement());
-        System.out.println(alfredBot.respondBeforeAlexis(
-            "Alexis, play some snake jazz"));
-        System.out.println(alfredBot.respondBeforeAlexis(
-            "Apologies, Alfred, play some snake jazz"));
-        System.out.println(alfredBot.respondBeforeAlexis(
-            "thank you"));
+        String testGreeting = alfredBot.basicGreeting();
+        String testGuestGreeting = alfredBot.guestGreeting("Lee", "morning");
+        String testDateAnnouncement = alfredBot.dateAnnouncement();
+        String alexisTest = alfredBot.respondBeforeAlexis(
+            "Alexis, play some snake jazz"
+        );
+        String alfredTest = alfredBot.respondBeforeAlexis(
+            "Apologies Alfred, play some snake jazz"
+        );
+        String defaultConvo = alfredBot.respondBeforeAlexis(
+            "Are you there?"
+        );
+
+        System.out.println(testGreeting);
+        System.out.println(testGuestGreeting);
+        System.out.println(testDateAnnouncement);
+        System.out.println(alexisTest);
+        System.out.println(alfredTest);
+        System.out.println(defaultConvo);
+        // System.out.println(alfredBot.guestGreeting("Lee", "morning"));
+        // System.out.println(alfredBot.dateAnnouncement());
+        // System.out.println(alfredBot.respondBeforeAlexis(
+        //     "Alexis, play some snake jazz"));
+        // System.out.println(alfredBot.respondBeforeAlexis(
+        //     "Apologies, Alfred, play some snake jazz"));
+        // System.out.println(alfredBot.respondBeforeAlexis(
+        //     "thank you" ));
     }
 }
